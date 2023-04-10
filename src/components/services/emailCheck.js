88c447f2872428bdac0fdda05ee3d80cae563d0a
vipnua <giangvipbg2002@gmail.com>
2023-04-10 14:49:24 +0700

    change api

--- src/components/services/emailCheck.js
+++ src/components/services/emailCheck.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const checkMailApi = createApi({
   reducerPath: 'checkMailApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://148.163.48.29:9000/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://107.189.159.137:9000/' }),
   endpoints: (builder) => ({
     checkMail: builder.mutation({
       query: (email) => ({
